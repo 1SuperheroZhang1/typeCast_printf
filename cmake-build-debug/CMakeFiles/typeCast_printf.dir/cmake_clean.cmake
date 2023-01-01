bf948f2622ac9152e09a34bb0ebc9a88d617342a
zhangcy <2304417358@qq.com>
2023-01-01 20:19:55 +0800

    C语言督学营 - 2.2 混合运算和printf函数

---
+++ cmake-build-debug/CMakeFiles/typeCast_printf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/typeCast_printf.dir/main.c.obj"
+  "libtypeCast_printf.dll.a"
+  "typeCast_printf.exe"
+  "typeCast_printf.exe.manifest"
+  "typeCast_printf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/typeCast_printf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/typeCast_printf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for typeCast_printf.
+# This may be replaced when dependencies are built.
